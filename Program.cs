c71a40d308b947815c774097ee3be3146869aa20
Lenovo <Lenovo@DESKTOP-HQH9Q0V>
2024-08-20 13:45:58 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.WriteLine("Por favor, ingresa tu nombre:");
+        string nombre = Console.ReadLine();
+
+     
+        if (!string.IsNullOrEmpty(nombre))
+        {
+         
+            char inicial = nombre[0];
+
+        
+            Console.WriteLine($"La inicial de tu nombre es: {inicial}");
+        }
+
+        }
+    }
+
